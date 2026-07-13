4faa92b6740453403634cdd89c3009bf1e5bdcda
fahmifikri2424-netizen <fahmifikri2424@gmail.com>
2026-07-13 19:39:04 +0700

    Delete test.php

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-require 'app/Config/Paths.php';
-$paths = new \Config\Paths();
-require $paths->systemDirectory . '/bootstrap.php';
-
-$db = \Config\Database::connect();
-$res = $db->query('SELECT id_booking, id_staff, id_schedule, tanggal_booking, status_booking FROM bookings')->getResultArray();
-print_r($res);
